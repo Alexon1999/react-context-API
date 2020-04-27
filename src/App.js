2d28a1fb6bd52f15aaef8ec989dde127a35d0ce0
Alexon1999 <alexon1999@gmail.com>
2020-04-27 19:08:38 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MovieList from './components/MovieList';
+import Nav from './components/nav';
+import { MovieProvider } from './context/MovieContext';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MovieProvider>
+      {/* Tout ce qui est dans MovieProvider auront tous les infos de Movies */}
+      <div className='App'>
+        <Nav />
+        <AddMovie />
+        <MovieList />
+      </div>
+    </MovieProvider>
   );
 }
 
